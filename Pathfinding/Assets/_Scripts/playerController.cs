f84d5889350cc4eba9a2caf7e3e3a5b7442ace55
Derek Mitchell <dmitchell356@gmail.com>
2014-10-31 03:00:17 -0400

    Movement fix and Energy Bolts

--- Pathfinding/Assets/_Scripts/playerController.cs
+++ Pathfinding/Assets/_Scripts/playerController.cs
@@ -51,11 +51,15 @@ public class playerController : MonoBehaviour {
 			headingTangent = transform.right;
 		}
 		
-		rigidbody.AddForce(-transform.up * (50f*transform.position.y + rigidbody.velocity.y));
+//		rigidbody.AddForce(-Vector3.up*(5f*transform.position.y + 5f*rigidbody.velocity.y));
 		
 		Vector3 slideDrag = -Vector3.Dot(headingTangent,rigidbody.velocity)*headingTangent;
 		slideDrag.y = 0f;
 		rigidbody.AddForce(slideDrag);
+
+		rigidbody.AddTorque(0f,turn * ang_vel,0f);
+//		rigidbody.AddTorque(10f * transform.rotation.x * transform.forward + 10f * transform.rotation.z * transform.right);
+//		Debug.Log(new Vector3(rectifyAngle(transform.eulerAngles.x),0f,rectifyAngle(transform.eulerAngles.z)));
 		
 		//Debug.Log (rigidbody.velocity);
 //		rigidbody.position = new Vector3 (
@@ -65,12 +69,17 @@ public class playerController : MonoBehaviour {
 //			Mathf.Clamp (rigidbody.position.z, boundary.zMin, boundary.zMax)
 //			);
 		
-		rigidbody.rotation = Quaternion.Euler (0.0f, rigidbody.rotation.eulerAngles.y + turn * ang_vel, turn * -tilt);
+//		rigidbody.rotation = Quaternion.Euler (0.0f, rigidbody.rotation.eulerAngles.y + turn * ang_vel, turn * -tilt);
 		
 //		GameObject.Find("Exhaust/OuterCore").particleEmitter.worldVelocity = rigidbody.velocity;
 //		GameObject.Find("Exhaust/InnerCore").particleEmitter.worldVelocity = rigidbody.velocity;
 	}
-	
-	
-	
+
+	float rectifyAngle(float ang){
+		while(ang >= 180)
+			ang -= 360;
+		while(ang < -180)
+			ang += 360;
+		return ang;
+	}
 }
