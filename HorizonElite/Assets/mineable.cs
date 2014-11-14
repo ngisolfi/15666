35ed21bc3b3778b6385c582ebd25a8d6fe9dfe9b
Nick Gisolfi <nick.gisolfi@gmail.com>
2014-11-14 12:53:50 -0500

    Added potential UI mechanism for mining progress notification

---
+++ HorizonElite/Assets/mineable.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mineable : MonoBehaviour {
+
+//
+//	// Use this for initialization
+//	void Start () {
+//	
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+//
+//	void OnTriggerStay( Collider other){
+//
+//		other.GetComponent<oreCapacity> ().Update_deuterium (.01);
+//
+//	}
+}
