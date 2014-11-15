908c20e52a0d985f08208e4caae0ba87bb939f57
Nick Gisolfi <nick.gisolfi@gmail.com>
2014-11-15 00:31:31 -0500

    Starting networking

--- HorizonElite/Assets/_Scripts/spawnAsteroidField.cs
+++ HorizonElite/Assets/_Scripts/spawnAsteroidField.cs
@@ -1,11 +1,12 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class spawnAsteroidField : MonoBehaviour {
 
 	public GameObject[] asteroids;
 	public int number;
-	public int radius;
+	//public int radius;
 
 	public float rotationSpeed;
 	public float driftSpeed;
@@ -13,14 +14,50 @@ public class spawnAsteroidField : MonoBehaviour {
 	public int lowScale;
 	public int highScale;
 
+	public Vector3 center;
+	public Vector3 spread;
+
+	private List<Transform> positions;
 
 	// Spawn a full field
 	void Start () {
-	
+
+		positions = new List<Transform>();
+		int bad_counter = 0;
+
 		for(int i=0;i<number; i++){
+
+			// If we have unsuccessfully attempted to spawn too many asteroids,
+			// just stop trying and move on with the game
+			if (bad_counter > number / 2)
+				break;
+
 			GameObject temp = Instantiate(asteroids[(int)Random.Range(0,3)],
-			                   Random.insideUnitSphere*radius,
-			                   Quaternion.LookRotation (Random.onUnitSphere*radius))  as GameObject;
+			                   /*Random.insideUnitSphere*radius*/BoxMullerRand(center, spread),
+			                   Quaternion.LookRotation (Random.onUnitSphere))  as GameObject;
+
+			// Check for collisions with other asteroids
+			bool spawn = true;
+			Vector3 extents1 = temp.renderer.bounds.extents;
+			float radius1 = Mathf.Max(extents1.x, Mathf.Max (extents1.y, extents1.z));
+			for (int j=0;j<positions.Count;++j)
+			{
+				Vector3 extents2 = positions[j].renderer.bounds.extents;
+				float radius2 = Mathf.Max(extents2.x, Mathf.Max (extents2.y, extents2.z));
+				Vector3 delta = temp.transform.position - positions[j].position;
+
+				if (delta.magnitude < radius1 + radius2)
+					spawn = false;
+			}
+
+			// If we collide with another asteroid, decrement and try from a new position
+			if (!spawn)
+			{
+				bad_counter++;
+				i--;
+				continue;
+			}
+
 			float scale = Random.Range (lowScale,highScale);
 			temp.transform.localScale = new Vector3(scale,scale,scale);
 			temp.rigidbody.mass = scale;
@@ -28,11 +65,26 @@ public class spawnAsteroidField : MonoBehaviour {
 //			temp.transform.localScale.z = scale;
 			temp.rigidbody.angularVelocity = Random.insideUnitCircle*rotationSpeed;
 //			temp.rigidbody.velocity = Random.Range (0,driftSpeed)*Random.onUnitSphere;
+
+			positions.Add(temp.transform);
 		}
 	}
 	
 	// spawn on side to fill field as it travels
-	void Update () {
-	
+	Vector3 BoxMullerRand(Vector3 mean, Vector3 var){
+		float u1 = Random.value;
+		float u2 = Random.value;
+		float a1 = Mathf.Sqrt(-2.0f * Mathf.Log(u1)) * Mathf.Sin(2.0f * Mathf.PI * u2);
+
+		float u3 = Random.value;
+		float u4 = Random.value;
+		float a2 = Mathf.Sqrt(-2.0f * Mathf.Log(u3)) * Mathf.Sin(2.0f * Mathf.PI * u4);
+
+		float u5 = Random.value;
+		float u6 = Random.value;
+		float a3 = Mathf.Sqrt(-2.0f * Mathf.Log(u5)) * Mathf.Sin(2.0f * Mathf.PI * u6);
+
+		return new Vector3(mean.x + var.x * a1, mean.y + var.y * a2, mean.z + var.z * a3);
+
 	}
 }
---
+++ HorizonElite/Assets/networkManager.cs
@@ -0,0 +1,117 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System;
+
+
+public class networkManager : MonoBehaviour {
+
+	private const string typeName = "Horizon Elite";
+	private const string gameName = "Universe 1";
+
+	private HostData[] hostList;
+
+	public GameObject alien_ship;
+	public GameObject human_ship;
+	private GameObject ship;
+	public GameObject cam;
+
+	void OnServerInitialized(){
+	}
+
+	void OnGUI(){
+	     //Game Title
+          GUIStyle titleFont = new GUIStyle();
+          titleFont.fontSize = 100;
+          titleFont.normal.textColor = Color.white;
+
+			if(!Network.isClient && !Network.isServer){
+				GUI.Label ( new Rect(100,100,100,50),"Horizon Elite",titleFont);
+				if(GUI.Button (new Rect (100,300,100,50),"Start Server"))
+					StartServer();
+				if(GUI.Button (new Rect(100,500,100,50),"Refresh Hosts"))
+					RefreshHostList();
+				if(hostList!=null){
+					for(int i=0;i<hostList.Length;i++){
+						if(GUI.Button (new Rect(400,100+(110*i),300,100),hostList[i].gameName))
+							JoinServer(hostList[i]);
+					}
+				}
+			}
+
+	}
+
+	private void RefreshHostList(){
+		MasterServer.RequestHostList (typeName);
+	}
+
+	void OnMasterServerEvent(MasterServerEvent msEvent){
+		if (msEvent == MasterServerEvent.HostListReceived)
+			hostList = MasterServer.PollHostList ();
+	}
+	
+	private void JoinServer(HostData hostData){
+		Network.Connect (hostData);
+		
+	}
+	
+	void OnConnectedToServer( ){
+		Spawn ();
+	}
+
+	private void StartServer(){
+		
+		Network.InitializeServer (4, 25000, !Network.HavePublicAddress ());
+		MasterServer.RegisterHost(typeName,gameName);
+
+		Spawn ();
+	}
+	
+	private void Spawn(){
+		if (Network.isServer)
+			ship = human_ship;
+		else
+			ship = alien_ship;
+
+		// Determine a spawn location and instantiate a new ship of the player's type
+		Vector3 spawn_location = GetSpawnLocation();
+		Quaternion spawn_direction = GetSpawnDirection ();
+		GameObject spawned = (GameObject)Network.Instantiate(ship, 
+		                                                     spawn_location, 
+		                                                     spawn_direction, 
+		                                                     networkView.GetInstanceID());
+
+		GameObject cam_spawned = (GameObject)Network.Instantiate (cam,
+		             		        							  spawn_location,
+		                    		                              spawn_direction,
+		                            		                      networkView.GetInstanceID ());
+
+		cam_spawned.GetComponent<cameraFollow> ().target = spawned.transform;
+		
+		// If the spawn was successful, set the camera to point to the spawned object
+		/*if (spawned)
+		{
+			if(Camera.main)
+				Camera.main.enabled = false;
+			Transform spawned_camera = transform.Find("Camera");
+			if (spawned_camera)
+			{
+				spawned_camera.camera.GetComponent<SmoothFollowCSharp>().target = spawned.transform;
+				spawned_camera.camera.enabled = true;
+			}
+			
+			active_ships.Add(spawned);
+		}*/
+	}
+
+	private Vector3 GetSpawnLocation()
+	{
+		return Vector3.zero;
+	}
+
+	private Quaternion GetSpawnDirection()
+	{
+		return Quaternion.identity;
+	}
+
+}
