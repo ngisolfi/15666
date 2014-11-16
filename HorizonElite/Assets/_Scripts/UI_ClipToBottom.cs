6a55585d767873ccf41b98a14fff36c5ca3357a0
Derek Mitchell <dmitchell356@gmail.com>
2014-11-15 22:16:46 -0500

    Added clip to left and bottom.

---
+++ HorizonElite/Assets/_Scripts/UI_ClipToBottom.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_ClipToBottom : MonoBehaviour {
+	public int offset;
+	private Camera UIcamera;
+	
+	// Use this for initialization
+	void Start () {
+		foreach(Transform sibling in transform.parent){
+			if(sibling.name == "Camera"){
+				UIcamera = sibling.camera;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 uiPos = transform.position;
+		uiPos.y = ((2f*(float)offset-Screen.height)/(float)Screen.height)*UIcamera.orthographicSize+renderer.bounds.extents.y;
+		transform.position = uiPos;
+	}
+}
---
+++ HorizonElite/Assets/_Scripts/UI_ClipToLeft.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_ClipToLeft : MonoBehaviour {
+	public int offset;
+	private Camera UIcamera;
+	
+	// Use this for initialization
+	void Start () {
+		foreach(Transform sibling in transform.parent){
+			if(sibling.name == "Camera"){
+				UIcamera = sibling.camera;
+			}
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 uiPos = transform.position;
+		uiPos.x = ((2f*(float)offset-Screen.width)/(float)Screen.height)*UIcamera.orthographicSize+renderer.bounds.extents.x;
+		transform.position = uiPos;
+	}
+}
