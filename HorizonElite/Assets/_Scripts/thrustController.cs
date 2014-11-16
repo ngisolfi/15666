d9a7f9c6c60e10a3b4f1c8a772bb44d76f21163f
Derek Mitchell <dmitchell356@gmail.com>
2014-11-15 21:24:20 -0500

    Fix inputs to use unity general inputs.  Also lasers fire at set rate when button is held.

--- HorizonElite/Assets/_Scripts/thrustController.cs
+++ HorizonElite/Assets/_Scripts/thrustController.cs
@@ -6,6 +6,8 @@ public class thrustController : MonoBehaviour {
 	public float speed;
 	public float pitchSpeed;
 	public float rollSpeed;
+	public float fireRate = 0.5F;
+	private float nextFire = 0.0F;
 
 	private Vector3 thrusterLocation;
 	private laserFire laserSpawn;
@@ -18,35 +20,32 @@ public class thrustController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-				if (networkView.isMine) {
-						if (Input.GetMouseButton (0)) {
-								//Thrust On
-								rigidbody.AddForce (speed * transform.forward);//(transform.position - thrusterLocation).normalized);
-						}
-						if (Input.GetMouseButtonDown (1)) {
-								laserSpawn.fireLaser ();
+		if (networkView.isMine) {
+			if (Input.GetButton("Fire1")) {
+				//Thrust On
+				rigidbody.AddForce (speed * transform.forward);//(transform.position - thrusterLocation).normalized);
+			}
+			if (Input.GetButton("Fire2") && Time.time > nextFire) {
+				nextFire = Time.time + fireRate;
+				laserSpawn.fireLaser ();
+			}
+			if (Input.GetAxis("Vertical") > 0f) {
 
-						}
-						if (Input.GetKey (KeyCode.W)) {
-
-								rigidbody.AddRelativeTorque (new Vector3 (pitchSpeed, 0, 0));
-								//rotate to fly down
-						}
-						if (Input.GetKey (KeyCode.S)) {
-								//rotate to fly up
-								rigidbody.AddRelativeTorque (new Vector3 (-pitchSpeed, 0, 0));
-
-						}
-						if (Input.GetKey (KeyCode.A)) {
-								//roll to the left
-								rigidbody.AddRelativeTorque (new Vector3 (0, 0, rollSpeed));
-						}
-						if (Input.GetKey (KeyCode.D)) {
-								//roll to the right
-								rigidbody.AddRelativeTorque (new Vector3 (0, 0, -rollSpeed));
-						}
-
-
-				}
+				rigidbody.AddRelativeTorque (new Vector3 (pitchSpeed, 0, 0));
+				//rotate to fly down
+			}
+			if (Input.GetAxis("Vertical") < 0f) {
+				//rotate to fly up
+				rigidbody.AddRelativeTorque (new Vector3 (-pitchSpeed, 0, 0));
+			}
+			if (Input.GetAxis("Horizontal") < 0f) {
+				//roll to the left
+				rigidbody.AddRelativeTorque (new Vector3 (0, 0, rollSpeed));
+			}
+			if (Input.GetAxis("Horizontal") > 0f) {
+				//roll to the right
+				rigidbody.AddRelativeTorque (new Vector3 (0, 0, -rollSpeed));
+			}
 		}
+	}
 }
