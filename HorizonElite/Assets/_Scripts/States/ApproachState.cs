52ea0db6f4a0e830b26c94a268d9dc3999a1ba05
unknown <dmitchell356@gmail.com>
2014-12-03 15:02:50 -0500

    Preliminary AI nonsense.

---
+++ HorizonElite/Assets/_Scripts/States/ApproachState.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ApproachState : State {
+	public State idle_state;
+	public State player_controlled_state;
+
+	public override void execute ()
+	{
+		if(_handler.goal){
+			Vector3 diff = _handler.goal.position-_controller.transform.position;
+			_controller.pitch(AngleSigned(_controller.transform.forward,diff,_controller.transform.right));
+			_controller.roll(-AngleSigned(_controller.transform.up,diff,_controller.transform.forward));
+			if(Vector3.Angle(_controller.transform.forward,diff) < 40f)
+				_controller.thrust();
+		}
+	}
+	
+	public override State transitionNext ()
+	{
+		if(_handler.playerControlled)
+			return player_controlled_state;
+		else if(!_handler.goal)
+			return idle_state;
+		else
+			return base.transitionNext ();
+	}
+}
---
+++ HorizonElite/Assets/_Scripts/States/StateHandler.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateHandler : MonoBehaviour {
+	private State _currentState;
+	public bool playerControlled;
+	public Transform goal;
+
+	// Use this for initialization
+	void Start () {
+		_currentState = transform.Find("States").GetComponent<IdleState>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void FixedUpdate () {
+		if (networkView.isMine) {
+			_currentState.execute();
+			// Check for transition to next state, continue transitioning until stablizes (beware of infinite loops)
+			State previous;
+			do{
+				previous = _currentState;
+				_currentState = previous.transitionNext();
+			}while(previous != _currentState);
+			
+		}
+	}
+}
